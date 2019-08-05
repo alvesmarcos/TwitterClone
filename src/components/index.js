57afc4e8adce02d3df4d233aa34882eb09c23d05
alvesmarcos <alves.henrique.marcos@gmail.com>
2019-08-05 00:25:08 -0300

    feat: add HorizontalSeparator component

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,13 @@ import IconWrapper from './IconWrapper';
 import Toolbar from './Toolbar';
 import TouchableSearchBar from './TouchableSearchBar';
 import Tweet from './Tweet';
+import HorizontalSeparator from './HorizontalSeparator';
 
-export { AvatarImage, IconWrapper, Toolbar, TouchableSearchBar, Tweet };
+export {
+  AvatarImage,
+  IconWrapper,
+  Toolbar,
+  TouchableSearchBar,
+  Tweet,
+  HorizontalSeparator,
+};
